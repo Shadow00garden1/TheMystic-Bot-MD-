160502c3d7aee282f7fb9c32e917f2bed3a682af
Shadow00garden1 <btprfuv810@hakikibal.xyz>
2024-09-21 17:31:44 +0300

    Create المطور.js

---
+++ المطور.js
@@ -0,0 +1,40 @@
+const handler = async (m, {conn, usedPrefix}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
+  const tradutor = _translate.plugins.info_creador
+
+  const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+  const document = doc[Math.floor(Math.random() * doc.length)];
+  const text = `*—◉ الرقم الأول للمالك wa.me/994406674437*
+
+  *—◉ الرقم التاني للمالك wa.me/994407254134*`.trim();
+  const buttonMessage= {
+    'document': {url: `https://github.com/InuYashahime/TheMystic-Bot-MD/tree/master`},
+    'mimetype': `application/${document}`,
+    'fileName': `${tradutor.texto2[0]}`,
+    'fileLength': 99999999999999,
+    'pageCount': 200,
+    'contextInfo': {
+      'forwardingScore': 200,
+      'isForwarded': true,
+      'externalAdReply': {
+        'mediaUrl': 'https://github.com/BrunoSobrino/TheMystic-Bot-MD',
+        'mediaType': 2,
+        'previewType': 'pdf',
+        'title': tradutor.texto2[1],
+        'body': wm,
+        'thumbnail': imagen1,
+        'sourceUrl': 'https://www.youtube.com/channel/UCSTDMKjbm-EmEovkygX-lCA'}},
+    'caption': text,
+    'footer': wm,
+   // 'buttons':[
+    // {buttonId: `${usedPrefix}menu`, buttonText: {displayText: '𝙼𝙴𝙽𝚄'}, type: 1},
+    // {buttonId: `${usedPrefix}donar`, buttonText: {displayText: '𝙳𝙾𝙽𝙰𝚁'}, type: 1}],
+    'headerType': 6};
+  conn.sendMessage(m.chat, buttonMessage, {quoted: m});
+};
+handler.help = ['owner', 'creator'];
+handler.tags = ['info'];
+handler.command = /^(owner|creator|creador|propietario|المالك|المطور)$/i;
+export default handler;
